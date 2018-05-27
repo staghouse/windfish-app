0b5deea5234da3211450713673445dd775700df5
Eric Lakatos <ericlakatos@outlook.com>
2018-05-26 20:12:45 -0400

    Additional IO files I might have missed?

---
+++ io/index.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+    const server = require('http').createServer(this.nuxt.renderer.app);
+    const io = require('socket.io')(server);
+
+    // overwrite nuxt.listen()
+    this.nuxt.listen = (port, host) =>
+        new Promise(resolve =>
+            server.listen(port || 3000, host || 'localhost', resolve)
+        );
+    // close this server on 'close' event
+    this.nuxt.plugin(
+        'close',
+        () => new Promise(resolve => server.close(resolve))
+    );
+
+    // Add `socket.io-client` in vendor
+    this.addVendor('socket.io-client');
+};
